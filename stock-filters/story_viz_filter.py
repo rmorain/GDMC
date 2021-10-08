149665a74576c981d59eb772d58eeef4cf128691
Robert Morain <robert.morain@gmail.com>
2021-10-08 11:42:22 -0600

    Notifies when village is built

--- stock-filters/story_viz_filter.py
+++ stock-filters/story_viz_filter.py
@@ -63,6 +63,7 @@ def perform(level, box, options):
     village_skeleton, terrain = create_minecraft_village(level, box, village_spec, num_evals=5, animate=False)
     build_road(level, box, terrain)
     build_village_skeleton(level, box, village_skeleton, terrain, village_spec.get_all_village_schematics())
+    print("Village built. Please enjoy :)")
 
 
 def build_village_skeleton(level, box, village_skeleton, terrain, schematic_files):
